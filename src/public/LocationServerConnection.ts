80534069aa9915908e4551db44aa3d933c847ed7
Glitch (typescript-fullstack) <none>
2020-04-28 20:24:40 +0000

    🐋📙 Checkpoint ./src/public/LocationServerConnection.ts:7701387/29

--- src/public/LocationServerConnection.ts
+++ src/public/LocationServerConnection.ts
@@ -33,7 +33,7 @@ export class LocationServerConnection {
   }
 
   public sendMovement(entity: Entity, movementDelta: Move) {
-    
+    console.log("send movement");
   }
 
   public sendMessage() {
