f6edcef885ed9e4979d86b51e1e28e8696406c68
Glitch (typescript-fullstack) <none>
2020-04-29 12:03:46 +0000

    📺🐾 Checkpoint ./src/public/client.ts:7701387/1343

--- src/public/client.ts
+++ src/public/client.ts
@@ -17,28 +17,27 @@ const usernameBox = document.getElementById("username") as HTMLInputElement;
 const connectButton = document.getElementById("connect") as HTMLButtonElement;
 usernameBox.value = uuidv4();
 
+const connection = new LocationServerConnection("wss://" + window.location.host); 
+connection.onMessageReceived((message) => {    
+  const knownEntity = worldContents.filter(item => item.id == message.sender.id).length > 0;
+  if (!knownEntity) {
+    worldContents.push(message.sender);  
+  }
+  const worldEntity = worldContents.filter(item => item.id == message.sender.id)[0];
+  worldEntity.x = message.sender.x;
+  worldEntity.y = message.sender.y;    
+});
+
 let worldContents: IDrawable[] = [];
-let connection: LocationServerConnection;
 let me: Attendee;
 
 function join() {
   worldContents = [];
-  connection = new LocationServerConnection("wss://" + window.location.host);  
-  connection.onMessageReceived((message) => {
-    
-    const knownEntity = worldContents.filter(item => item.id == message.sender.id).length > 0;
-    if (!knownEntity) {
-      worldContents.push(message.sender);  
-    }
-    const worldEntity = worldContents.filter(item => item.id == message.sender.id)[0];
-    worldEntity.x = message.sender.x;
-    worldEntity.y = message.sender.y;
-    
-  });
 
   const me = new Attendee(usernameBox.value, "room1", 50, 50);
   me.onMovement((entity, move) => connection.sendMovement(entity, move));
   
+  connection.join(me);
   worldContents.push(me);
   new Controls(me).connect();
 }
